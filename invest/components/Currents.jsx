b5ee58bbbfca48d1f95f84769f3d91f0b6041064
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-07-30 15:37:47 +0100

    Update Currents.jsx

--- invest/components/Currents.jsx
+++ invest/components/Currents.jsx
@@ -4,16 +4,16 @@ import CountUp from 'react-countup';
 
 export default function Currents({plan, initial, amount, nextPay, rate}) {
   const planColors = {
-    diamond: 'diamond-sh',
-    silver: 'silver-sh',
-    gold: 'gold-sh',
-    generational: 'gen-sh',
+    workers: 'diamond-sh',
+    college: 'silver-sh',
+    platinium: 'gold-sh',
+    retirement: 'gen-sh',
   };
   const planText = {
-    diamond: 'diamond-txt',
-    silver: 'silvertxt',
-    gold: 'gold-txt',
-    generational: 'gen-txt',
+    workers: 'diamond-txt',
+    college: 'silvertxt',
+    platinium: 'gold-txt',
+    retirement: 'gen-txt',
   };
 
   const bgCol = planColors[plan] || '';
@@ -21,10 +21,11 @@ export default function Currents({plan, initial, amount, nextPay, rate}) {
 
   return (
     <div className={`rounded-lg ${bgCol} p-5 flex flex-col gap-5 text-[#eee]`}>
-      <div className='text-[#eee]'>{`+${rate} in the next ${nextPay} day(s)`}</div>
+      <h1 className='text-sm text=[#a2a1ab]'>{plan}</h1>
+      <div className='text-[#eee] text-center'>{`+${rate} in the next ${nextPay} day(s)`}</div>
       <div className='flex flex-col justify-center items-center w-full gap-5'>
-        <div className='flex items-center gap-2 flex-wrap'>
-          <CountUp start={0} end={amount} duration={2} separator="," className='w-full font-bold text-lg'/>
+        <div className='flex items-center gap-2 flex-wrap w-full'>
+          <CountUp start={0} end={amount} duration={2} separator="," className='font-bold text-lg'/>
           <span className={`text-[11px] ${txtCol}`}>Current</span>
           </div>
         <div className='flex items-center gap-5 w-full'>
