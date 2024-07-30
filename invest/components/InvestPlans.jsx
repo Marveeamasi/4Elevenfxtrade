242c7c76c1a17875ff73c07902a880367c895af9
Marvellous Amasi <102649743+Marveeamasi@users.noreply.github.com>
2024-07-30 15:32:16 +0100

    Update InvestPlans.jsx

--- invest/components/InvestPlans.jsx
+++ invest/components/InvestPlans.jsx
@@ -7,10 +7,10 @@ export default function InvestPlans() {
 
   return (
     <div className='w-full flex flex-col justify-center max-sm:items-center p-4 sm:px-20 gap-20 sm:gap-10 mt-20'>
-        <h1>Select a plan to invest</h1>
+        <h1 className='text-3xl'>Select a plan to invest</h1>
     <div className='grid sm:grid-cols-2 grid-cols-1 gap-10 xl:grid-cols-3 2xl:grid-cols-4'>
       <div className='flex flex-col shadow-glow-mild rounded-lg w-full h-[500px] justify-center items-center gap-5 p-4'>
-         <div className='text-2xl font-bold text-center w-full'>Silver Plan</div>
+         <div className='text-2xl font-bold text-center w-full'>College Plan</div>
          <div className='text-3xl font-bold text-center w-full text-col '>5% Weekly ROI</div>
          <div className='flex gap-2 items-center  w-full'>
           <TiTickOutline className='text-[#00eaff]'/>
@@ -21,11 +21,11 @@ export default function InvestPlans() {
           <div className=''>Maximum Deposit - $2000</div>
          </div>
          <p className='text-center w-full text-[#a2a1ab]'>Every 2 weeks</p>
-         <Link href='payment/silver' className=' flex justify-center items-center w-40 h-10 rounded-lg bg-col hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
+         <Link href='payment/college' className=' flex justify-center items-center w-40 h-10 rounded-lg bg-col hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
  
       </div>
       <div className='flex flex-col bg-col rounded-lg w-full h-[500px] justify-center items-center gap-5 p-4 opacity-[.9]'>
-         <div className='text-2xl font-bold text-center w-full text-[#000]'>Generational Plan</div>
+         <div className='text-2xl font-bold text-center w-full text-[#000]'>Retirement Plan</div>
          <div className='text-3xl font-bold text-center w-full text-[#ffffffef]'>12% Weekly ROI</div>
          <div className='flex gap-2 items-center w-full'>
           <TiTickOutline className='text-[#000000]'/>
@@ -36,11 +36,11 @@ export default function InvestPlans() {
           <div className='text-black font-bold'>Maximum Deposit - $10,000,000</div>
          </div>
          <p className='text-center w-full text-[#181818] font-bold'>Duration of 1 year</p>
-         <Link href='payment/generational' className=' flex justify-center items-center w-40 h-10 rounded-lg border border-black hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
+         <Link href='payment/retirement' className=' flex justify-center items-center w-40 h-10 rounded-lg border border-black hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
  
       </div>
       <div className='flex flex-col shadow-glow-mild rounded-lg w-full h-[500px] justify-center items-center gap-5 p-4'>
-         <div className='text-2xl font-bold text-center w-full'>Gold Plan</div>
+         <div className='text-2xl font-bold text-center w-full'>Platinium Plan</div>
          <div className='text-3xl font-bold text-center w-full text-col '>9% Weekly ROI</div>
          <div className='flex gap-2 items-center  w-full'>
           <TiTickOutline className='text-[#00eaff]'/>
@@ -51,11 +51,11 @@ export default function InvestPlans() {
           <div className=''>Maximum Deposit - $300,000</div>
          </div>
          <p className='text-center w-full text-[#a2a1ab]'>Every 3 months</p>
-         <Link href='payment/gold' className=' flex justify-center items-center w-40 h-10 rounded-lg bg-col hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
+         <Link href='payment/platinium' className=' flex justify-center items-center w-40 h-10 rounded-lg bg-col hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
  
       </div>
       <div className='flex flex-col shadow-glow-mild rounded-lg w-full h-[500px] justify-center items-center gap-5 p-4'>
-         <div className='text-2xl font-bold text-center w-full'>Diamond Plan</div>
+         <div className='text-2xl font-bold text-center w-full'>Workers Plan</div>
          <div className='text-3xl font-bold text-center w-full text-col '>7% Weekly ROI</div>
          <div className='flex gap-2 items-center  w-full'>
           <TiTickOutline className='text-[#00eaff]'/>
@@ -66,7 +66,7 @@ export default function InvestPlans() {
           <div className=''>Maximum Deposit - $50,000</div>
          </div>
          <p className='text-center w-full text-[#a2a1ab]'>Every 1 month</p>
-         <Link href='payment/diamond' className=' flex justify-center items-center w-40 h-10 rounded-lg bg-col hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
+         <Link href='payment/workers' className=' flex justify-center items-center w-40 h-10 rounded-lg bg-col hover:opacity-75 text-black text-sm font-bold'>Invest</Link>
  
       </div>
     </div>
